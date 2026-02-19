2df6963dd59e5a52e3627add3ab7c9324e7351b8
Zytronium <dastelljes@outlook.com>
2026-02-19 13:54:19 -0600

    Extract terminal to a client component and add scanlines

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,25 @@
 import Image from "next/image";
 
+function Console() {
+  "use client";
+  return (
+    <div
+      className="flex flex-col scanlines items-start gap-1 rounded-2xl bg-zinc-950 border-1 border-zinc-700 w-[80vw] h-[80vh] p-4">
+      <span>zytronium@web:~$ echo Hello World!</span>
+      <span>Hello World!</span>
+    </div>
+  );
+}
+
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div
+      className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <div className="flex flex-row gap-1 justify-center items-center py-2">
         <Image src="/logo.png" width={24} height={24} alt="DATAPORT.exe logo" />
         <p className="text-zinc-400">DATAPORT.exe</p>
       </div>
-        <div className="flex flex-col items-start gap-1 rounded-xl bg-zinc-950 border-1 border-zinc-700 w-[80vw] h-[80vh] p-4">
-          <span>zytronium@web:~$ echo Hello World!</span>
-          <span>Hello World!</span>
-        </div>
+      <Console />
     </div>
   );
 }
